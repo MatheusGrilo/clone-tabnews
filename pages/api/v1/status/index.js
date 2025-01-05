d125945f43c0840a39fc8ff7694b3efe6a9dcf20
Matheus Grilo <MatheusGrilo@users.noreply.github.com>
2025-01-05 22:32:59 +0000

    add `database.js`, `compose.yaml` and `env.example`, changed `gitignore` and installed `pg`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../utils/database";
+
+async function status(request, response) {
+  const result = await database.query("select now() as time_now;");
+  console.log(result.rows[0]);
   response.status(200).json({ status: "Tudo funcionando 👍" });
 }
 
