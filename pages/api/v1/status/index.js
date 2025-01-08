831b5a8bd02d2f5c76dad3a7cf213f8a67214645
Matheus Grilo <MatheusGrilo@users.noreply.github.com>
2025-01-08 18:42:40 +0000

    `/api/v1/status` endpoint and tests from database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "utils/database";
 
 async function status(request, response) {
-  const result = await database.query("select now() as time_now;");
-  console.log(result.rows[0]);
-  response.status(200).json({ status: "Tudo funcionando 👍" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query(`SHOW server_version;`);
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query(`SHOW max_connections;`);
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
